29765f7c7a44a163cd2fdaf35a9f895fa4b6cfa5
rajtailor28 <rajtailor2871@gmail.com>
2024-07-20 19:04:24 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            firstName: '',
+            lastName: '',
+            quantity: 0,
+            pricePerItem: 10
+        };
+    },
+    computed: {
+        fullName(){
+            return `${this.firstName} ${this.lastName}`;
+        },
+        totalPrice(){
+            return this.quantity * this.pricePerItem;
+        }
+    },
+    
+    methods:{
+        clearFields(){
+            this.firstName='',
+            this.lastName ='',
+            this.quantity = 0
+        }
+       
+    }
+});
+app.mount('#app');
